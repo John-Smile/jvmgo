e1d422466d44cc6cb58dcbc19ef18154569c3487
hulkhuo@lexinfintech.com <hulkhuo@lexinfintech.com>
2017-07-19 20:22:15 +0800

    添加

--- ch02/main.go
+++ ch02/main.go
@@ -1,5 +1,7 @@
 package main
 import "fmt"
+import "strings"
+import "./classpath"
 func main() {
     cmd := parseCmd()
     if cmd.versionFlag {
@@ -12,5 +14,13 @@ func main() {
 }
 
 func startJVM(cmd *Cmd) {
+    cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
     fmt.Printf("classpath:%s class:%s args:%v\n", cmd.cpOption, cmd.class, cmd.args)
+    className := strings.Replace(cmd.class, ".", "/", -1)
+    classData, _, err := cp.ReadClass(className)
+    if err != nil {
+        fmt.Printf("Could not find or load main class %s\n", cmd.class)
+        return
+    }
+    fmt.Printf("class data:%v\n", classData)
 }
