d6bb7c3bf78bed1632857dfee03162706651a6dc
huomingkun <260587778@qq.com>
2017-07-27 01:12:06 +0800

    modify

--- ch06/rtda/heap/constant_pool.go
+++ ch06/rtda/heap/constant_pool.go
@@ -17,7 +17,35 @@ func newConstantPool(class *Class, cfCp classfile.ConstantPool)  *ConstantPool {
 	for i := 1; i < cpCount; i++ {
 		cpInfo := cfCp[i]
 		switch cpInfo.(type) {
-
+		case *classfile.ConstantIntegerInfo:
+			intInfo := cpInfo.(*classfile.ConstantIntegerInfo)
+			consts[i] = intInfo.Value()
+		case *classfile.ConstantFloatInfo:
+			floatInfo := cpInfo.(*classfile.ConstantFloatInfo)
+			consts[i] = floatInfo.Value()
+		case *classfile.ConstantLongInfo:
+			longInfo := cpInfo.(*classfile.ConstantLongInfo)
+			consts[i] = longInfo.Value()
+			i++
+		case *classfile.ConstantDoubleInfo:
+			doubleInfo := cpInfo.(*classfile.ConstantDoubleInfo)
+			consts[i] = doubleInfo.Value()
+			i++
+		case *classfile.ConstantStringInfo:
+			stringInfo := cpInfo.(*classfile.ConstantStringInfo)
+			consts[i] = stringInfo.String()
+		case *classfile.ConstantClassInfo:
+			classInfo := cpInfo.(*classfile.ConstantClassInfo)
+			consts[i] = newClassRef(rtCp, classInfo)
+		case *classfile.ConstantFieldrefInfo:
+			fieldrefInfo := cpInfo.(*classfile.ConstantFieldrefInfo)
+			consts[i] = newFieldRef(rtCp, fieldrefInfo)
+		case *classfile.ConstantMethodrefInfo:
+			methodrefInfo := cpInfo.(*classfile.ConstantMethodrefInfo)
+			consts[i] = newMethodRef(rtCp, methodrefInfo)
+		case *classfile.ConstantInterfaceMethodrefInfo:
+			methodrefInfo := cpInfo.(*classfile.ConstantInterfaceMethodrefInfo)
+			consts[i] = newInterfaceMethodRef(rtCp, methodrefInfo)
 		}
 	}
 	return rtCp
