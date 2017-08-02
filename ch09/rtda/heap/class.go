1a938afe65b5856e3f8810f44f3d5f823854abb4
hulkhuo@lexinfintech.com <hulkhuo@lexinfintech.com>
2017-08-02 20:58:46 +0800

    modify

--- ch09/rtda/heap/class.go
+++ ch09/rtda/heap/class.go
@@ -147,5 +147,6 @@ func (self *Class) JavaName() string  {
 	return strings.Replace(self.name, "/", ".", -1)
 }
 func (self *Class) IsPrimitive() bool  {
-	_, ok := primitiveTypes[self.]
+	_, ok := primitiveTypes[self.name]
+	return ok
 }
